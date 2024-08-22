89214488c53947777aedb00336475cfec818e071
El-hathat <mohamedelhathat9@gmail.com>
2024-08-22 18:49:15 +0200

    UI v0.0.8

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -10,10 +10,10 @@ function page() {
     <Image src={circle} alt=''   className='hidden lg:flex w-16 h-16 relative top-[50%] left-[70%]' />
 
     </div>
-      <div   className="lg:w-[70%] flex w-full max-w-sm mx-auto overflow-hidden bg-white bg-opacity-30  rounded-lg shadow-lg   lg:max-w-4xl">
+      <div   className="lg:w-[70%] flex w-full max-w-sm mx-auto overflow-hidden bg-white bg-opacity-30   rounded-lg shadow-lg   lg:max-w-4xl">
     <div   className="hidden bg-cover lg:block lg:w-1/2" style={{backgroundImage: 'url(https://img.freepik.com/photos-premium/fille-est-assise-devant-ordinateur-mot-mathematiques-tableau-noir_1104772-4194.jpg)'}}></div>
 
-    <div  className="w-full  max-w-sm mx-auto overflow-hidden bg-white bg-opacity-30  rounded-lg shadow-md  ">
+    <div  className="w-full  max-w-sm mx-auto overflow-hidden bg-white bg-opacity-30 lg:bg-opacity-0  rounded-lg shadow-md  ">
     <div  className="px-6 py-4">
         <div  className="flex justify-center mx-auto">
             <Image  className="w-[130px] h-[100px] " src={logo} alt=""/>
@@ -25,27 +25,27 @@ function page() {
 
         <form>
             <div  className="w-full mt-4">
-                <input  className="block w-full px-4 py-2 mt-2 text-white placeholder-gray-500 bg-white bg-opacity-30  border rounded-lg    focus: border-[#ff8367]  focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" />
+                <input  className="block w-full px-4 py-2 mt-2 text-white placeholder-gray-200 bg-white bg-opacity-30  border rounded-lg    focus: border-[#ff8367]  focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-[#ff8367]" type="email" placeholder="Email Address" aria-label="Email Address" />
             </div>
 
             <div  className="w-full mt-4">
-                <input  className="block w-full px-4 py-2 mt-2 text-white placeholder-gray-500 bg-white bg-opacity-30  border rounded-lg   focus: border-[#ff8367]  focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" />
+                <input  className="block w-full px-4 py-2 mt-2 text-white placeholder-gray-200 bg-white bg-opacity-30  border rounded-lg   focus: border-[#ff8367]  focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-[#ff8367]" type="password" placeholder="Password" aria-label="Password" />
             </div>
 
             <div  className="flex items-center justify-between mt-4">
                 <a href="#"  className="text-sm text-white  hover:text-white">Forget Password?</a>
 
-                <button  className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                <button  className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#ff8367] rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                     Sign In
                 </button>
             </div>
         </form>
     </div>
 
-    <div  className="flex items-center justify-center py-4 text-center bg-gray-50">
+    <div  className="flex items-center justify-center py-4 text-center ">
         <span  className="text-sm text-white ">Don't have an account? </span>
 
-        <a href="#"  className="mx-2 text-sm font-bold text-blue-500  hover:underline">Register</a>
+        <a href="#"  className="mx-2 text-sm font-bold text-[#ff8367]  hover:underline  ">Register</a>
     </div>
 </div>
 </div>
